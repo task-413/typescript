92fda81393c335f77ce892bf1a9b7a7de46c6b4c
Хубашвили Георгий Антонович ИП-513 <user@localhost.localdomain>
2026-03-17 11:24:07 +0700

    Need think about task5

--- src/task5.ts
+++ src/task5.ts
@@ -2,11 +2,14 @@ import { groupBy, having, sort, where } from "./task4.js";
 
 export type Transform<T> = (data: T[]) => T[];
 
-const enum  StepCategory (
-    Where = 'where',
-)
+const enum  StepCategory {
+    Where = 'where', GroupBy = 'groupBy', Having = 'having', Sort = 'sort'
+};
 
-export type WhereStep<T> = Transform<T> 
+export type WhereStep<T> = Transform<T> & { __category:StepCategory.Where };
+export type GroupByStep<T, K extends keyof T> = ((data: T[]) => Group<T, K>[]) & { __category: StepCategory.GroupBy };
+export type HavingStep<T, K extends keyof T> = ((groups: Group<T, K>[]) => Group<T, K>[]) & { __category: StepCategory.Having}
+
 
 export type Group<T, K extends keyof T> = {
     key: T[K];
@@ -14,6 +17,20 @@ export type Group<T, K extends keyof T> = {
 };
 
 export function where<T, K extends keyof T>(key: K, value: T[K]): WhereStep<T> {
-    const fn: Transform<T> = (data: T[] => data.filter(item => item[key] == value);
+    const fn: Transform<T> = (data: T[]) => data.filter(item => item[key] == value);
     return Object.assign(fn, { __category: StepCategory.Where as const});
 }
+
+export function groupBy<T, K extends keyof T>(key: K): GroupByStep<T, K> {
+
+}
+
+export function having<T, K extends keyof T>(predicate: (group: Group<T, K>) => boolean): HavingStep<T, K> {
+    
+}
+
+export function sort<T, K extends keyof T>(key: K): SortStep<T> {
+    
+}
+
+type StepCategory<S> = S extends { __category: infer C} ? C : never;
